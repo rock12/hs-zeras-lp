28718909205fcff2339ce2c2e024081904fad2e0
assusdan <assusdan@SRT.tk>
2015-09-19 17:36:33 +0500

    leds

--- arch/arm/mach-mt6582/s5501/leds/mt65xx/cust_leds.c
+++ arch/arm/mach-mt6582/s5501/leds/mt65xx/cust_leds.c
@@ -118,7 +118,7 @@ static struct cust_mt65xx_led cust_led_list[MT65XX_LED_TYPE_TOTAL] = {
 	{"jogball-backlight", MT65XX_LED_MODE_NONE, -1,{0}},
 	{"keyboard-backlight",MT65XX_LED_MODE_NONE, -1,{0}},
 	{"button-backlight",  MT65XX_LED_MODE_PMIC, MT65XX_LED_PMIC_NLED_ISINK2,{0}},
-	{"lcd-backlight",     MT65XX_LED_MODE_CUST_BLS_PWM, (int)disp_bls_set_backlight,{0}},
+	{"lcd-backlight",     MT65XX_LED_MODE_CUST_BLS_PWM, (int)disp_bls_set_backlight,{0,1,0,0,0}},
 };
 
 struct cust_mt65xx_led *get_cust_led_list(void)
